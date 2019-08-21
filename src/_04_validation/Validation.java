4867a3d5492a1f09c2ca2c5ca9783163a4e3988d
ilabstaff <ilabstaff@172.16.116.79>
2019-08-20 17:57:08 -0700

    no

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -9,6 +9,29 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			
+			
+			if (randomNumber == 0) {
+							JOptionPane.showMessageDialog(null, "I hate you");
+						} else if (randomNumber == 2) {
+							JOptionPane.showMessageDialog(null, "you are suckey");
+						} else if (randomNumber == 3) {
+							JOptionPane.showMessageDialog(null, "it is a butifll day out side flowers are blooming brds are singing on days like this people like you sude be brening in hell");
+						} else if (randomNumber == 4) {
+							JOptionPane.showMessageDialog(null,"............................." );
+						} else if (randomNumber == 5) {
+						JOptionPane.showMessageDialog(null,"no no you  suck at every thing even at life");	
+						} else {
+							
+						}
+					}	
+		
+		
+		
+		
+		
+		
 		
 		Random randomMaker = new Random();
 		
@@ -16,10 +39,23 @@ public class Validation {
 		
 		System.out.println(randomNumber);
 
+		
 		// 1. Use each value of randomNumber to give the user a random compliment.
 
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
+	
+		// 2. Fix the code below so that it displays the correct image
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 }
